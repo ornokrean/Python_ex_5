70cb994c3324b933b0cdf3e021b98d66e3f45d3c
maayanch <maayan.chetrit96@gmail.com>
2018-05-31 16:52:15 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/filesprocessing/FileParsing.java

--- src/filesprocessing/Orders/testerOrder.java
+++ src/filesprocessing/Orders/testerOrder.java
@@ -9,28 +9,32 @@ import java.util.Arrays;
 
 public class testerOrder {
 
-    //files :
 
     File c = new File("c.txt");
-    File bh= new File("bh.docs");
-    File bs=new File("bs.mp4");
-    File k= new File("k.txt");
-    File l =new File("l");
-    File re =new File("re");
+    File bh = new File("bh.docs");
+    File bs = new File("bs.mp4");
+    File k = new File("k.txt");
+    File l = new File("l.txt");
+    File re = new File("re.a");
 
-    //files arrays:
+    File[] notSorted = {c, bh, k, l, bs, re};
+    File[] sortedByName = {bh, bs, c, k, l, re};
 
-    File[] notSorted={c,bh,k,l,bs,re};
-    File[] sortedByName={bh,bs,c,k,l,re};
+    String path ="C:\\testex5files\\advanced_source_directory";
+    File f = new File(path);
+    File[] notSortedFromFile = f.listFiles();
+    File[] sortedByNameFromFiles=
 
+
+    File[] sortedByType = {}
 
 
     @Test
-    public void testOrderAbs(){
+    public void testOrderAbs() {
 
         AbsOrder comp = new AbsOrder(false);
         Arrays.sort(notSorted, comp);
-        Assert.assertEquals("not sorted well",notSorted ,  sortedByName  );
+        Assert.assertEquals("not sorted well", notSorted, sortedByName);
     }
 }
 
