2771e9f35ca50d427053c2b852331086558c3f20
or <or.nokrean@mail.huji.ac.il>
2018-05-31 13:52:42 +0300

    nothing

--- src/filesprocessing/DirectoryProcessor.java
+++ src/filesprocessing/DirectoryProcessor.java
@@ -9,6 +9,6 @@ public class DirectoryProcessor {
 	static String PATH = "/Users/or/Desktop/CS/Java/ex5/src/filesprocessing/testfile";
 
 	public static void main(String[] args) throws IOException {
-
+//aa
 	}
 }
