44d7843db06d0a079042c19b90239423da78fcd6
maayanch <maayan.chetrit96@gmail.com>
2018-06-01 15:48:08 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/filesprocessing/FileParsing.java

--- src/filesprocessing/Orders/SizeOrder.java
+++ src/filesprocessing/Orders/SizeOrder.java
@@ -1,13 +1,24 @@
 package filesprocessing.Orders;
 
 import java.io.File;
-
+/**
+ * Class extends Order, determines the hierarchy between two files by the size of the file. if two files
+ * have the same size, the hierarchy will be determined by the default.
+ */
 class SizeOrder extends Order {
 
 
+    /**
+     * abstract method that determines the hierarchy between two files by the size
+     *
+     * @param file1 - file one
+     * @param file2 - file two
+     * @return - zero if the files are equal , positive number if file one bigger than file two ,
+     * negative number if file two bigger than file one
+     */
     @Override
     public int compare(File file1, File file2) {
         long dif = file1.length() - file2.length();
-        return (dif == 0) ? defaultCompare(file1, file2) : (int) dif;
+        return (dif == EQUAL) ? defaultCompare(file1, file2) : (int) dif;
     }
 }
