c4fd3a71509ef9ec195c00abc701d2b237439551
or <or.nokrean@mail.huji.ac.il>
2018-06-03 11:52:15 +0300

    upper_bound >= lower_bound

--- src/filesprocessing/filters/BetweenFilter.java
+++ src/filesprocessing/filters/BetweenFilter.java
@@ -6,14 +6,8 @@ public class BetweenFilter extends Filter {
 	private static final int SIZE_FACTOR = 1024;
 
 	public boolean passFilter(File f, String[] args) throws FilterException{
-		long upper_bound = (long) Double.parseDouble(args[1].replace(" ",""));
-		long lower_bound = (long) Double.parseDouble(args[2].replace(" ",""));
-		System.out.println("srgs between :");
-		for (String s : args){
-			System.out.println(s);
-
-		}
-		System.out.println("done args");
+		long upper_bound = (long) Double.parseDouble(args[2].replace(" ",""));
+		long lower_bound = (long) Double.parseDouble(args[1].replace(" ",""));
 		if (args.length != 3 || lower_bound < 0 || upper_bound < lower_bound) {
 			throw new FilterException();
 		}
