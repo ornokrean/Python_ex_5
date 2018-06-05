c842258b0f7da69df59b1fe5eba3fcd231ee5b17
or <or.nokrean@mail.huji.ac.il>
2018-06-05 16:12:36 +0300

    bound in between

--- src/filesprocessing/filters/BetweenFilter.java
+++ src/filesprocessing/filters/BetweenFilter.java
@@ -6,17 +6,20 @@ import java.io.File;
 public class BetweenFilter extends Filter {
 	private static final double SIZE_FACTOR = 1024.0;
 
+	private double upper_bound;
+	private double lower_bound;
+
 	public boolean passFilter(File file, String[] args) {
 
 		double size = file.length() / SIZE_FACTOR;
-		double upper_bound = Double.parseDouble(args[2].replace(" ", ""));
-		double lower_bound = Double.parseDouble(args[1].replace(" ", ""));
-		return (lower_bound <= size && size <= upper_bound);
+//		this.upper_bound = Double.parseDouble(args[2].replace(" ", ""));
+//		this.lower_bound = Double.parseDouble(args[1].replace(" ", ""));
+		return (this.lower_bound <= size && size <= this.upper_bound);
 	}
 	@Override
 	public boolean checkCommand(String[] command) throws FilterException{
-		double upper_bound = Double.parseDouble(command[2].replace(" ", ""));
-		double lower_bound = Double.parseDouble(command[1].replace(" ", ""));
+		this.upper_bound = Double.parseDouble(command[2].replace(" ", ""));
+		this.lower_bound = Double.parseDouble(command[1].replace(" ", ""));
 		if (command.length != 3 || lower_bound < 0 || upper_bound < lower_bound) {
 			throw new FilterException();
 		}
